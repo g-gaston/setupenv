c6dd3ebc177beadb2558e99f65f798a120243e0c
Guillermo Gaston <gaslor@amazon.com>
2024-06-24 17:25:52 -0500

    Add new bootstrap script for ansible refactor

---
+++ bootstrap.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# success prints a checkmark followed by the first argument
+success() {
+  printf "\U2705 %s\n" "$1"
+}
+
+# esnure_command checks if a set of commands are available and installs them if not
+ensure_command() {
+  for cmd in "$@"; do
+    if ! command -v "$cmd" &> /dev/null; then
+      case "$(uname)" in
+        Darwin)
+          brew install "$cmd"
+          success "$1 installed"
+          ;;
+        Linux) # For linux, assume ubuntu
+          sudo apt install -y "$cmd"
+          ;;
+        *)
+          return 1
+          ;;
+      esac
+      success "$1 installed"
+    else
+      success "$1 already installed"
+    fi
+  done
+}
+
+GITHUB_SSH_KEY_PATH=$HOME/.ssh/github
+DEVENV_PATH=$HOME/devenv
+
+# If Mac install hombrew,
+if [ "$(uname)" == "Darwin" ]; then
+  if ! command -v brew &> /dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    success "brew installed"
+  else
+    success "$1 already installed"
+  fi
+elif [ "$(uname)" == "Linux" ]; then
+  sudo apt update
+fi
+
+ensure_command ansible gh
+
+# Generate ssh key for github and store it in .ssh folder if it doesn't exist
+[ -f "$GITHUB_SSH_KEY_PATH" ] || ssh-keygen -t ed25519 -C "" -f "$GITHUB_SSH_KEY_PATH" -N ""
+
+# Setup github auth if not set yet
+if ! gh auth status > /dev/null 2>&1; then
+  # TODO: maybe use the token base auth for machine without graphical interface
+  gh auth login --git-protocol ssh --host github.com --web
+else
+  success "gh is already authenticated"
+fi
+
+# Clone devenv repo with ansible playbooks
+[ -f "$DEVENV_PATH" ] || git clone git@github.com:g-gaston/devenv.git "$DEVENV_PATH"
+
+# Install ansible extra modules
+ansible-galaxy install -r "$DEVENV_PATH/ansible/requirements.yml"
+
+# Run ansible playbook with args from this comand
+ansible-playbook "$DEVENV_PATH/ansible/main.yml" --ask-become-pass "$@"
